0092897a354166791f7699756343932a1dcacb4e
Aka Konin <aka.konin@gmail.com>
2026-06-24 16:53:13 +0000

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Exécuté par Render à chaque déploiement (voir render.yaml).
-# set -o errexit
+set -o errexit
 
-# # Force the use of uv to install packages into Render's active virtual environment
-# uv pip install -r requirements.txt
+# Force the use of uv to install packages into Render's active virtual environment
+uv pip install -r requirements.txt
 
-# # Run your Django management commands safely
-# python manage.py collectstatic --noinput
-# python manage.py migrate
-# python manage.py ensure_superuser
+# Run your Django management commands safely
+python manage.py migrate
+python manage.py ensure_superuser
+python manage.py collectstatic --noinput
 
-uv pip install -r requirements.txt && .venv/bin/python manage.py migrate && .venv/bin/python manage.py ensure_superuser && .venv/bin/python manage.py collectstatic --noinput
+# uv pip install -r requirements.txt && .venv/bin/python manage.py migrate && .venv/bin/python manage.py ensure_superuser && .venv/bin/python manage.py collectstatic --noinput
