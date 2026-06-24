a59c5826f61bbc99f917f6ade379b17599bd91cd
Aka Konin <aka.konin@gmail.com>
2026-06-24 15:58:35 +0000

    fix: use uv pip install in build script

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 # Exécuté par Render à chaque déploiement (voir render.yaml).
 set -o errexit
 
-pip install -r requirements.txt
+# Force the use of uv to install packages into Render's active virtual environment
+uv pip install -r requirements.txt
 
+# Run your Django management commands safely
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py ensure_superuser
